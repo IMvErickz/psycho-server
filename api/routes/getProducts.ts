1f874933e9c5e82bd7fa73f791b66f445a2a67e4
IMvErickz <erickspy2003@gmail.com>
2023-01-20 21:31:40 -0300

    Added route for get products informations for ID and NAME

--- api/routes/getProducts.ts
+++ api/routes/getProducts.ts
@@ -1,10 +1,55 @@
 import { prisma } from "../../lib/prisma";
 import {FastifyInstance} from 'fastify'
+import { z } from "zod";
 
 export async function GetProducts(fastify: FastifyInstance) {
     fastify.get('/products', async () => {
         const products = await prisma.products.findMany()
 
         return{products}
+    })
+
+    fastify.get('/products/description/:id', async (request) => {
+
+        const productName = z.object({
+            id: z.string(),
+        })
+
+        const {id} = productName.parse(request.params)
+
+        const products = await prisma.products.findUnique({
+            where: {
+                id
+            },
+            select: {
+                Name: true,
+                description: true,
+                price: true,
+            }
+        })
+
+        return {products}
+
+    })
+
+    fastify.get('/products/nameAll/:Name', async (request) => {
+
+        const getProductName = z.object({
+            Name: z.string()
+        })
+
+        const {Name} = getProductName.parse(request.params)
+
+        const products = await prisma.products.findMany({
+            where: {
+                Name
+            },
+            select: {
+                Name: true,
+                price: true
+            }
+        })
+
+        return {products}
     })
 }
